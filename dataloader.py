9ec59181dcf385660bd1cb9b62628914d88d2616
Mrrrat <mrrrat@yandex.ru>
2022-04-10 00:13:18 +0300

    change

--- dataloader.py
+++ dataloader.py
@@ -2,6 +2,7 @@ import os
 import torchaudio
 import pandas as pd
 import numpy as np
+import torch
 from torch.utils.data import Dataset, DataLoader, WeightedRandomSampler
 from sklearn.model_selection import train_test_split
 
@@ -30,6 +31,18 @@ class AccentDataset(Dataset):
         return len(self.audios)
 
 
+def collate_fn(batch):
+    data_list, label_list = [], []
+    max_size = 0
+    for _data, _label in batch:
+        max_size = max(max_size, len(_data))
+        data_list.append(_data)
+        label_list.append(_label)
+    data_list = [torch.cat([item,torch.tensor([0]).expand(max_size- len(item))]) for item in data_list]
+    data_list = torch.cat([item[None] for item in data_list])
+    return data_list, torch.LongTensor(label_list)
+
+
 def get_data_loaders(data_dir, meta_path, transform=None, bs=512):
     meta = pd.read_csv(meta_path)
     train_idx, val_idx, _, _ = train_test_split(np.arange(meta.shape[0], dtype=int), meta['target'], test_size=0.2,
@@ -40,7 +53,7 @@ def get_data_loaders(data_dir, meta_path, transform=None, bs=512):
     weights = 1.0 / meta.loc[train_idx, 'count'].values
     sampler = WeightedRandomSampler(weights, num_samples=len(weights))
 
-    train_loader = DataLoader(train_dataset, batch_size=bs, num_workers=0, pin_memory=True,
+    train_loader = DataLoader(train_dataset, batch_size=bs, collate_fn=collate_fn, num_workers=0, pin_memory=True,
                               drop_last=True, sampler=sampler)
     val_loader = DataLoader(val_dataset, batch_size=1, num_workers=0, pin_memory=True)
     return train_loader, val_loader
